71a6bcccd5bbf688558e4b16c6a64bdd8bb492e6
Vlad4774 <vladbaracut7@yahoo.com>
2022-10-05 13:37:06 +0300

    String is not null or empty

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -6,12 +6,17 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
-            return HaveQuotes(input);
+            return !IsNullOrEmpty(input) && HaveQuotes(input);
         }
 
         static bool HaveQuotes(string input)
         {
             return input[0] == '"' && input[input.Length - 1] == '"' && input.Length > 1;
+        }
+
+        static bool IsNullOrEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
         }
     }
 }
