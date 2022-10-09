9437850f0455690b3c09c4f24325822110900da8
Vlad4774 <vladbaracut7@yahoo.com>
2022-10-09 13:06:41 +0300

    I checked if the number has letters

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -6,12 +6,25 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return !IsNotNullOrEmpty(input);
+            return !IsNotNullOrEmpty(input) && !ContainLetters(input);
         }
 
         static bool IsNotNullOrEmpty(string input)
         {
             return string.IsNullOrEmpty(input);
+        }
+
+        static bool ContainLetters(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z'))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
