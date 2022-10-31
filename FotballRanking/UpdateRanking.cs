fa212ddab60fbe1090abb70a0a63aaac7c93febd
Vlad4774 <vladbaracut7@yahoo.com>
2022-10-31 10:05:34 +0200

    changes

--- FotballRanking/UpdateRanking.cs
+++ FotballRanking/UpdateRanking.cs
@@ -3,28 +3,26 @@ namespace FotballRanking
 {
     class UpdateRanking
     {
-        Team team;
-        private int goals;
-        public UpdateRanking(Team team, int goals)
+        Team[] teams;
+        public UpdateRanking(Team[] teams)
         {
-            this.team = team;
-            this.goals = goals;
+            this.teams = teams;
         }
 
-        public void Update(UpdateRanking first, UpdateRanking second)
+        public void Score(Team first, Team second, int firstGoals, int secondGoals)
         {
-            if (first.goals > second.goals)
+            if (firstGoals > secondGoals)
             {
-                first.team.AddPoints(team, 3);
+                first.AddPoints(first, 3);
             }
-            else if (first.goals < second.goals)
+            else if (firstGoals < secondGoals)
             {
-                second.team.AddPoints(team, 3);
+                second.AddPoints(second, 3);
             }
             else
             {
-                first.team.AddPoints(team, 1);
-                second.team.AddPoints(team, 1);
+                first.AddPoints(first, 1);
+                second.AddPoints(second, 1);
             }
         }
     }
---
+++ FotballRanking/UpdateRankingTest.cs
@@ -0,0 +1,37 @@
+﻿namespace FotballRanking
+{
+    public class UpdateRankingTest
+    {
+        [Fact]
+        public void SecondTeamWinsAgainstFirstTeam()
+        {
+            Team[] teams = new Team[0];
+            var ranking = new Ranking(teams);
+            var newTeam = new Team("fcsb", 5);
+            ranking.Add(newTeam, ref teams);
+            newTeam = new Team("sepsi", 3);
+            ranking.Add(newTeam, ref teams);
+            var updateRanking = new UpdateRanking(teams);
+            updateRanking.Score(teams[0], teams[1], 2, 3);
+            ranking.SortRanking(ref teams);
+            Assert.Equal(1, ranking.PositionOf(newTeam, teams));
+        }
+
+        [Fact]
+        public void ThirdTeamWinsMatch()
+        {
+            Team[] teams = new Team[0];
+            var ranking = new Ranking(teams);
+            var newTeam = new Team("fcsb", 17);
+            ranking.Add(newTeam, ref teams);
+            newTeam = new Team("Sepsi", 18);
+            ranking.Add(newTeam, ref teams);
+            newTeam = new Team("Rapid", 19);
+            ranking.Add(newTeam, ref teams);
+            var updateRanking = new UpdateRanking(teams);
+            updateRanking.Score(teams[0], teams[2], 1, 3);
+            ranking.SortRanking(ref teams);
+            Assert.Equal(2, ranking.PositionOf(newTeam, teams));
+        }
+    }
+}
