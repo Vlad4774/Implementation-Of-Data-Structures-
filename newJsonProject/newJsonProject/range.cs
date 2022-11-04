2eca8903c2319e4eb0ba38e10bc4f43ac6d72d8a
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-04 11:37:45 +0200

    Range Class

---
+++ newJsonProject/newJsonProject/range.cs
@@ -0,0 +1,32 @@
+﻿
+namespace newJsonProject
+{
+    class Range
+    {
+        char start;
+        char end;
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            foreach (var c in text)
+            {
+                if (c < start || c > end)
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/rangeTests.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace newJsonProject
+{
+    public class rangeTests
+    {
+        [Theory]
+        [InlineData("abc")]
+        [InlineData("fab")]
+        [InlineData("bcd")]
+
+        public void CasesTrue(string text)
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match(text));
+
+        }
+
+        [Theory]
+        [InlineData("1ab")]
+        [InlineData("")]
+        [InlineData(null)]
+
+        public void CaseFalse(string text)
+        {
+            var digit = new Range('a', 'f');
+            Assert.False(digit.Match(text));
+        }
+    }
+}
