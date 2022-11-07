af75533d381c626c6f58f269d501c99b36f97cbd
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-07 10:44:06 +0200

    class choice

---
+++ newJsonProject/newJsonProject/choice.cs
@@ -0,0 +1,26 @@
+﻿
+namespace newJsonProject
+{
+    interface IPattern
+    {
+        bool Match(string text);
+    }
+    class Choice
+    {
+        IPattern[] patterns;
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Match(string text)
+        {
+            foreach (var pattern in patterns)
+            {
+                return pattern.Match(text);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/choiseTests.cs
@@ -0,0 +1,14 @@
+﻿
+namespace newJsonProject
+{
+    public class choiseTests
+    {
+        [Fact]
+
+        public void CasesTrue()
+        {
+            var digit = new Choice (new Character('0'),
+                                   new Range('1', '9') );
+        }
+    }
+}
