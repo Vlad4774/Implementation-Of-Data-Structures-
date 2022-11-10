af144e60b014a5ea27c21b405b57584e5f4a725d
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-10 19:31:03 +0200

    I made the class Many

---
+++ newJsonProject/newJsonProject/Many.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace newJsonProject
+{
+    class Many : IPattern
+    {
+        IPattern pattern;
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+    }
+
+        public IMatch Match(string text)
+        {
+            while (pattern.Match(text).Success())
+            {
+                text = text.Substring(1);
+            }
+
+            return new Match(true, text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/ManyTests.cs
@@ -0,0 +1,27 @@
+﻿
+namespace newJsonProject
+{
+    public class ManyTests
+    {
+        [Theory]
+        [InlineData("abc", "bc")]
+        [InlineData("aaaabc", "bc")]
+        [InlineData("bc", "bc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void CaseswithCharacter(string text, string result)
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("12345ab123", "ab123")]
+        [InlineData("ab", "ab")]
+        public void CaseswithRange(string text, string result)
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.Equal(result, digits.Match(text).RemainingText());
+        }
+    }
+}
