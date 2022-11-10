7b119cb3706c298703bcd60fd7fd5c0dd707c83b
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-10 22:42:42 +0200

    I made class Optional

---
+++ newJsonProject/newJsonProject/optional.cs
@@ -0,0 +1,17 @@
+﻿
+namespace newJsonProject
+{
+    class Optional : IPattern
+    {
+        IPattern pattern;
+        public Optional(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text).Success() ? new Match(true, text.Substring(1)) : new Match(true, text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/optionalTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newJsonProject
+{
+    public class optionalTests
+    {
+        [Theory]
+        [InlineData("abc", "bc")]
+        [InlineData("aabc", "abc")]
+        [InlineData("bc", "bc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void CasesWithA(string text, string result)
+        {
+            var a = new Optional(new Character('a'));
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("123", "123")]
+        [InlineData("-123", "123")]
+        public void CasesWithSign(string text, string result)
+        {
+            var sign = new Optional(new Character('-'));
+            Assert.Equal(result, sign.Match(text).RemainingText());
+        }
+    }
+}
