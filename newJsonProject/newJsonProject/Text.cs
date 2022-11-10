52b69bea95ba071616994ca2422e76f147fdbea5
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-10 19:16:40 +0200

    I made the class Text

---
+++ newJsonProject/newJsonProject/Text.cs
@@ -0,0 +1,30 @@
+﻿
+namespace newJsonProject
+{
+    class Text
+    {
+        string prefix;
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }    
+
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                if (prefix[i] != text[i])
+                {
+                    return new Match(false, text);
+                }
+            }
+
+            return new Match(true, text.Substring(prefix.Length));
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/textTests.cs
@@ -0,0 +1,34 @@
+﻿
+namespace newJsonProject
+{
+    public class textTests
+    {
+        [Theory]
+        [InlineData("true", "true", "")]
+        [InlineData("true", "trueX", "X")]
+        [InlineData("false", "false", "")]
+        [InlineData("false", "falseX", "X")]
+        [InlineData("", "true", "true")]
+        public void CasesTrue(string prefix, string text, string result)
+        {
+            var parameter = new Text(prefix);
+            Assert.True(parameter.Match(text).Success());
+            Assert.Equal(result, parameter.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("true", "false", "false")]
+        [InlineData("true", "", "")]
+        [InlineData("true", null, null)]
+        [InlineData("false", "true", "true")]
+        [InlineData("false", "", "")]
+        [InlineData("false", null, null)]
+        [InlineData("", null, null)]
+        public void CasesFalse(string prefix, string text, string result)
+        {
+            var parameter = new Text(prefix);
+            Assert.False(parameter.Match(text).Success());
+            Assert.Equal(result, parameter.Match(text).RemainingText());
+        }
+    }
+}
