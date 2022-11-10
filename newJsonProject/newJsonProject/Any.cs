264b2b0c5e9a5ae05168657e329136ffe0471eee
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-10 18:54:35 +0200

    i made class any

---
+++ newJsonProject/newJsonProject/Any.cs
@@ -0,0 +1,30 @@
+﻿
+namespace newJsonProject
+{
+    class Any : IPattern
+    {
+        string accepted;
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            foreach (char c in accepted)
+            {
+                if (c == text[0])
+                {
+                    return new Match(true, text.Substring(1));
+                }
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/AnyTests.cs
@@ -0,0 +1,32 @@
+﻿
+namespace newJsonProject
+{
+    public class AnyTests
+    {
+        [Theory]
+        [InlineData("eE", "ea", "a")]
+        [InlineData("eE", "Ea", "a")]
+        [InlineData("-+", "+3", "3")]
+        [InlineData("-+", "-2", "2")]
+        public void CasesTrue(string accepted, string text, string result)
+        {
+            var e = new Any(accepted);
+            Assert.True(e.Match(text).Success());
+            Assert.Equal(result, e.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("eE", "a", "a")]
+        [InlineData("eE", "", "")]
+        [InlineData("eE", null, null)]
+        [InlineData("-+", "2", "2")]
+        [InlineData("-+", "", "")]
+        [InlineData("-+", null, null)]
+        public void CasesFalse(string accepted, string text, string result)
+        {
+            var e = new Any(accepted);
+            Assert.False(e.Match(text).Success());
+            Assert.Equal(result, e.Match(text).RemainingText());
+        }
+    }
+}
