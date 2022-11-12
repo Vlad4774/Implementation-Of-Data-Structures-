c967cd73451bf15bcd375afaa4f5099fa1756733
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-12 13:55:18 +0200

    I made the class OneOrMore

---
+++ newJsonProject/newJsonProject/OneOrMore.cs
@@ -0,0 +1,18 @@
+﻿
+namespace newJsonProject
+{
+    class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Sequence(pattern, new Many(pattern));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/OneOrMoreTests.cs
@@ -0,0 +1,30 @@
+﻿
+namespace newJsonProject
+{
+    public class OneOrMoreTests
+    {
+        [Theory]
+        [InlineData("123", "")]
+        [InlineData("1a", "a")]
+
+        void CasesTrue(string text, string result)
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.True(a.Match(text).Success());
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("bc", "bc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+
+
+        void CasesFalse(string text, string result)
+        {
+            var a = new OneOrMore(new Range('0', '9'));
+            Assert.False(a.Match(text).Success());
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+    }
+}
