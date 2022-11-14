74a99a2b5cda4756f90a73e4ca80efbf8e4ffdb7
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-14 16:07:03 +0200

    class List

---
+++ newJsonProject/newJsonProject/List.cs
@@ -0,0 +1,21 @@
+﻿
+namespace newJsonProject
+{
+    class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Optional(
+                new Sequence(
+                    element,
+                    new Many(new Sequence(separator, element))));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/ListTests.cs
@@ -0,0 +1,36 @@
+﻿
+namespace newJsonProject
+{
+    public class ListTests
+    {
+        [Theory]
+        [InlineData("1,2,3", "")]
+        [InlineData("1,2,3,", ",")]
+        [InlineData("1a", "a")]
+        [InlineData("abc", "abc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+
+        public void FirstCases(string text, string result)
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+            Assert.True(a.Match(text).Success());
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("1; 22  ;\n 333 \t; 22", "")]
+        [InlineData("1 \n;", " \n;")]
+        [InlineData("abc", "abc")]
+
+        public void JsonCases(string text, string result)
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+            Assert.True(list.Match(text).Success());
+            Assert.Equal(result, list.Match(text).RemainingText());
+        }
+    }
+}
