67782e773812a2d1ec60065b5eb54a478897c721
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-15 15:21:41 +0200

    class number

---
+++ newJsonProject/newJsonProject/number.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace newJsonProject
+{
+    class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var integer = new Choice(new Sequence(new Character('-'), digits),
+                                 new Sequence(digits));
+            var fraction = new Optional(new Sequence(new Character('.'), digits));
+            var sign = new Optional(new Choice(new Character('-'), new Character('+')));
+            var exponent = new Any("eE");
+            var exponentPart = new Optional(new Sequence(exponent, sign, digits));
+            pattern = new Sequence(integer, fraction, exponentPart);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/numberTests.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace newJsonProject
+{
+    public class numberTests
+    {
+        [Theory]
+        [InlineData("0")]
+        [InlineData("76")]
+        [InlineData("986412")]
+        public void CanBeAPozitiveNumber(string text)
+        {
+            var number = new Number();
+            Assert.True(number.Match(text).Success());
+        }
+
+        [Theory]
+        [InlineData("-2", "")]
+        [InlineData("-345", "")]
+        [InlineData("-986412", "")]
+        public void CanBeANegativeNumber(string text, string result)
+        {
+            var number = new Number();
+            Assert.Equal(result, number.Match(text).RemainingText());
+            Assert.True(number.Match(text).Success());
+        }
+
+        [Theory]
+        [InlineData("-2.321", "")]
+        [InlineData("-345.121", "")]
+        [InlineData("0.1231", "")]
+        public void CanBeAFractionalNumber(string text, string result)
+        {
+            var number = new Number();
+            Assert.Equal(result, number.Match(text).RemainingText());
+            Assert.True(number.Match(text).Success());
+        }
+
+        [Theory]
+        [InlineData("-2.321e+12", "")]
+        [InlineData("3E3", "")]
+        [InlineData("0.1231e+12", "")]
+        [InlineData("-345.121E1", "")]
+        public void CanBeAExponantialNumber(string text, string result)
+        {
+            var number = new Number();
+            Assert.Equal(result, number.Match(text).RemainingText());
+            Assert.True(number.Match(text).Success());
+        }
+    }
+}
