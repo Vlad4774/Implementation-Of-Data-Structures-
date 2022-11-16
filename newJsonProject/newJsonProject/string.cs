d205e1f3477c5ee6fd1e02e0c43be8dcdb0fc7a9
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-16 20:32:03 +0200

    first try for class string

---
+++ newJsonProject/newJsonProject/string.cs
@@ -0,0 +1,24 @@
+﻿
+namespace newJsonProject
+{
+    class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            var hex = new Choice(new Range('A', 'Z'),
+                                 new Range('a', 'z'),
+                                 new Range('0', '9'));
+            var unicode = new Sequence(new Character('u'), hex, hex, hex, hex);
+            var escapedCharacters = new Sequence(new Character('\\'), new Choice(unicode, new Any("\"\\/bfnrst")));
+            var characters = new Choice(hex, escapedCharacters);
+            pattern = new Sequence(new Character('"'), new OneOrMore(characters), new Character('"'));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ newJsonProject/newJsonProject/stringTests.cs
@@ -0,0 +1,48 @@
+﻿
+namespace newJsonProject
+{
+    public class stringTests
+    {
+        [Fact]
+        public void IsDoubleQuoted()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted("abc")).Success());
+            Assert.Equal("", str.Match(Quoted("abc")).RemainingText());
+        }
+
+        [Fact]
+        public void AlwaysStartsWithQuotes()
+        {
+            var str = new String();
+            Assert.False(str.Match("abc\"").Success());
+            Assert.Equal("abc\"", str.Match("abc\"").RemainingText());
+        }
+
+        [Fact]
+        public void HasStartAndEndQuotes()
+        {
+            var str = new String();
+            Assert.False(str.Match("\"").Success());
+            Assert.Equal("\"", str.Match("\"").RemainingText());
+        }
+
+        [Fact]
+        public void DoesNotContainControlCharacters()
+        {
+            var str = new String();
+            Assert.False(str.Match(Quoted("a\nb\rc")).Success());
+            Assert.Equal(Quoted("a\nb\rc"), str.Match(Quoted("a\nb\rc")).RemainingText());
+        }
+
+        [Fact]
+        public void CanContainLargeUnicodeCharacters()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted("⛅⚾")).Success());
+            Assert.Equal("", str.Match(Quoted("⛅⚾")).RemainingText());
+        }
+        public static string Quoted(string text)
+    => $"\"{text}\"";
+    }
+}
