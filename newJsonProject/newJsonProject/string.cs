769ad07132521e90cecad20fd2c3824c127d5541
Vlad4774 <vladbaracut7@yahoo.com>
2022-11-17 17:00:11 +0200

    class string

--- newJsonProject/newJsonProject/string.cs
+++ newJsonProject/newJsonProject/string.cs
@@ -10,9 +10,12 @@ namespace newJsonProject
             var hex = new Choice(new Range('A', 'Z'),
                                  new Range('a', 'z'),
                                  new Range('0', '9'));
+            var accepted = new Choice(new Range(' ', '!'),
+                                      new Range('#', '\u005B'),
+                                      new Range('\u005D', '\uFFFF'));
             var unicode = new Sequence(new Character('u'), hex, hex, hex, hex);
             var escapedCharacters = new Sequence(new Character('\\'), new Choice(unicode, new Any("\"\\/bfnrst")));
-            var characters = new Choice(hex, escapedCharacters);
+            var characters = new Choice(escapedCharacters, accepted);
             pattern = new Sequence(new Character('"'), new OneOrMore(characters), new Character('"'));
         }
 
--- newJsonProject/newJsonProject/stringTests.cs
+++ newJsonProject/newJsonProject/stringTests.cs
@@ -8,7 +8,6 @@ namespace newJsonProject
         {
             var str = new String();
             Assert.True(str.Match(Quoted("abc")).Success());
-            Assert.Equal("", str.Match(Quoted("abc")).RemainingText());
         }
 
         [Fact]
@@ -16,7 +15,6 @@ namespace newJsonProject
         {
             var str = new String();
             Assert.False(str.Match("abc\"").Success());
-            Assert.Equal("abc\"", str.Match("abc\"").RemainingText());
         }
 
         [Fact]
@@ -24,7 +22,6 @@ namespace newJsonProject
         {
             var str = new String();
             Assert.False(str.Match("\"").Success());
-            Assert.Equal("\"", str.Match("\"").RemainingText());
         }
 
         [Fact]
@@ -32,7 +29,6 @@ namespace newJsonProject
         {
             var str = new String();
             Assert.False(str.Match(Quoted("a\nb\rc")).Success());
-            Assert.Equal(Quoted("a\nb\rc"), str.Match(Quoted("a\nb\rc")).RemainingText());
         }
 
         [Fact]
@@ -40,8 +36,58 @@ namespace newJsonProject
         {
             var str = new String();
             Assert.True(str.Match(Quoted("⛅⚾")).Success());
-            Assert.Equal("", str.Match(Quoted("⛅⚾")).RemainingText());
         }
+
+        [Fact]
+        public void CanContainEscapedQuotationMark()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted(@"\""a\"" b")).Success());
+        }
+
+        [Fact]
+        public void CanContainEscapedBackspace()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted(@"a \b b")).Success());
+        }
+
+        [Fact]
+        public void CanContainEscapedLineFeed()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted(@"a \n b")).Success());
+        }
+
+        [Fact]
+        public void CanContainEscapedUnicodeCharacters()
+        {
+            var str = new String();
+            Assert.True(str.Match(Quoted(@"a \u26Be b")).Success());
+        }
+
+        [Fact]
+        public void DoesNotContainUnrecognizedExcapceCharacters()
+        {
+            var str = new String();
+            Assert.False(str.Match(Quoted(@"a\x")).Success());
+        }
+
+        [Fact]
+        public void DoesNotEndWithReverseSolidus()
+        {
+            var str = new String();
+            Assert.False(str.Match(Quoted(@"a\")).Success());
+        }
+
+        [Fact]
+        public void DoesNotEndWithAnUnfinishedHexNumber()
+        {
+            var str = new String();
+            Assert.False(str.Match(Quoted(@"a\u")).Success());
+            Assert.False(str.Match(Quoted(@"a\u123")).Success());
+        }
+
         public static string Quoted(string text)
     => $"\"{text}\"";
     }
