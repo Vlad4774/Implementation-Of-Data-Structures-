412a28d7f8f2a44af892447b1ed0239504f69086
Vlad4774 <vladbaracut7@yahoo.com>
2022-12-08 14:04:37 +0200

    I made IntArray

---
+++ Collection/Collection/IntArray.Tests.cs
@@ -0,0 +1,154 @@
+namespace Collection
+{
+    public class IntArrayTests
+    {
+        [Fact]
+        public void AddNumber()
+        {
+            var array = new IntArray();
+            array.Add(10);
+            Assert.True(array.Contains(10));
+        }
+
+        [Fact]
+        public void AddNumbers() 
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            Assert.Equal(1, array.IndexOf(23));
+        }
+
+        [Fact]
+        public void LengthIfArrayIsEmpty()
+        {
+            var array = new IntArray();
+            Assert.Equal(0, array.Count());
+        }
+
+        [Fact]
+        public void LengthIfArrayHaveNumbers()
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            Assert.Equal(3, array.Count());
+        }
+
+        [Fact]
+        public void ElementFromPosition()
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            Assert.Equal(1, array.Element(2));
+        }
+
+        [Theory]
+        [InlineData(2, 34)]
+        [InlineData(1, 12)]
+        [InlineData(0, 50)]
+        public void SetElementFromPosition(int index, int element)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.SetElement(index, element);
+            Assert.Equal(element, array.Element(index));
+        }
+
+        [Theory]
+        [InlineData(67, true)]
+        [InlineData(23, true)]
+        [InlineData(13, false)]
+        [InlineData(97, false)]
+        public void ContainsElement(int element, bool result)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.Add(12);
+            Assert.Equal(result, array.Contains(element));
+        }
+
+        [Theory]
+        [InlineData(67, 3)]
+        [InlineData(23, 1)]
+        [InlineData(13, -1)]
+        [InlineData(97, -1)]
+        public void IndexOfElement(int element, int index)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.Add(12);
+            Assert.Equal(index, array.IndexOf(element));
+        }
+
+        [Theory]
+        [InlineData(67, 3)]
+        [InlineData(23, 1)]
+        [InlineData(13, 2)]
+        [InlineData(97, 0)]
+        public void InsertElement(int element, int index)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.Insert(index, element);
+            Assert.Equal(index, array.IndexOf(element));
+        }
+
+        [Fact]
+        public void ClearArray()
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.Clear();
+            Assert.Equal(0, array.Count());
+        }
+
+        [Theory]
+        [InlineData(67)]
+        [InlineData(23)]
+        [InlineData(1)]
+        [InlineData(7)]
+        public void RemoveElement(int element)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.Remove(element);
+            Assert.Equal(3, array.Count());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void RemoveElementFromIndex(int index)
+        {
+            var array = new IntArray();
+            array.Add(7);
+            array.Add(23);
+            array.Add(1);
+            array.Add(67);
+            array.RemoveAt(index);
+            Assert.Equal(3, array.Count());
+        }
+    }
+}
---
+++ Collection/Collection/intArray.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Collection
+{
+    class IntArray
+    {
+        int[] numbers;
+
+	    public IntArray()
+        {
+            this.numbers = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize(ref numbers, numbers.Length + 1);
+            numbers[numbers.Length - 1] = element;
+        }
+
+        public int Count()
+        {
+            return numbers.Length;
+        }
+
+        public int Element(int index)
+        {
+            return numbers[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            numbers[index] = element;
+        }
+
+        public bool Contains(int element)
+        {
+            foreach(int number in numbers)
+            {
+                if (number == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(int element)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, int element)
+        {
+            Array.Resize(ref numbers, numbers.Length + 1);
+            for (int i = numbers.Length - 1; i > index; i--)
+            {
+                numbers[i] = numbers[i - 1];
+            }
+
+            numbers[index] = element;
+        }
+
+        public void Clear()
+        {
+            numbers = new int[0]; 
+        }
+
+        public void Remove(int element)
+        {
+            int index = 0;
+            while(index < numbers.Length)
+            {
+                if (numbers[index] == element)
+                {
+                    break;
+                }
+
+                index++;
+            }
+
+            RemoveAt(index);
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = numbers.Length - 1; i > index; i--)
+            {
+                numbers[i - 1] = numbers[i];
+            }
+
+            Array.Resize(ref numbers, numbers.Length - 1);
+        }
+
+    }
+}
