3e2bf737ba6d81db4b67e4780992f239f01d76f3
Vlad4774 <vladbaracut7@yahoo.com>
2022-12-14 19:09:55 +0200

    class List

---
+++ Collection/Collection/List.Tests.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Collection
+{
+    public class ListTests
+    {
+        [Fact]
+        public void AddElements()
+        {
+            var list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert.Equal(3, list.Count);
+            Assert.True(list.Contains(3));
+        }
+
+        [Fact]
+        public void InsertElements()
+        {
+            var list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Insert(1, "4");
+            Assert.Equal("4", list[1]);
+        }
+
+        [Fact]
+        public void CountIfIsAEmpty()
+        {
+            var list = new List<string>();
+            Assert.Equal(0, list.Count);
+        }
+
+        [Theory]
+        [InlineData(2, 34)]
+        [InlineData(1, 12)]
+        [InlineData(0, 50)]
+        public void SetElementFromPosition(int index, int element)
+        {
+            var list = new List<int>();
+            list.Add(7);
+            list.Add(23);
+            list.Add(1);
+            list[index] = element;
+            Assert.Equal(element, list[index]);
+        }
+
+    }
+}
---
+++ Collection/Collection/List.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+
+namespace Collection
+{
+    class List<T> : IEnumerable
+    {
+        protected T[] objects;
+
+        public List()
+        {
+            this.objects = new T[4];
+        }
+
+        public void Add(T element)
+        {
+            EnoughCapacity();
+            objects[Count] = element;
+            Count++;
+        }
+
+        private void EnoughCapacity()
+        {
+            if (objects.Length == Count)
+            {
+                Array.Resize(ref objects, objects.Length * 2);
+            }
+        }
+
+        public int Count { get; private set; } = 0;
+
+        public T this[int index]
+        {
+            get => objects[index];
+            set => objects[index] = value;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) >= 0;
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                if (objects[i].Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T element)
+        {
+            EnoughCapacity();
+            ShiftRight(index);
+            objects[index] = element;
+            Count++;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = objects.Length - 1; i > index; i--)
+            {
+                objects[i] = objects[i - 1];
+            }
+        }
+
+        public void Clear()
+        {
+            objects = new T[4];
+            Count = 0;
+        }
+
+        public void Remove(T element)
+        {
+            int index = FindIndex(element);
+            RemoveAt(index);
+        }
+
+        private int FindIndex(T element)
+        {
+            for (int index = 0; index < objects.Length; index++)
+            {
+                if (objects[index].Equals(element))
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = objects.Length - 1; i > index; i--)
+            {
+                objects[i - 1] = objects[i];
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return objects[i];
+            }
+        }
+    }
+}
