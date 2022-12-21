25f4c16e5696c7d1c3b8622ef25803f74625d1fe
Vlad4774 <vladbaracut7@yahoo.com>
2022-12-21 12:16:15 +0200

    write in a file with stream

---
+++ stream/stream/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class Write
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\vladb\\OneDrive\\Documente\\file.txt"))
+            {
+                writer.WriteLine("Am reusit");
+            }
+        }
+    }
+}
