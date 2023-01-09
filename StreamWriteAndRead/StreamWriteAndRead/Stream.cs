34ae55b9031d415394e5907a98b01173babe714d
Vlad4774 <vladbaracut7@yahoo.com>
2023-01-09 13:00:30 +0200

    encrypted and compress

--- StreamWriteAndRead/StreamWriteAndRead/Stream.cs
+++ StreamWriteAndRead/StreamWriteAndRead/Stream.cs
@@ -1,21 +1,42 @@
 ﻿
+using System.IO.Compression;
+using System.Security.Cryptography;
+
 namespace WorkWithStream
 {
-    class StreamWriteAndRead
+    public class StreamWriteAndRead
     {
         private MemoryStream stream;
-        private StreamReader reader;
-        private StreamWriter writer;
+        private bool encrypted;
+        private bool compressed;
+        private byte[] key;
+        private byte[] iv;
 
-        public StreamWriteAndRead(MemoryStream newStream)
+        public StreamWriteAndRead(MemoryStream newStream, bool encrypt, bool compress)
         {
             this.stream = newStream;
-            reader = new StreamReader(stream);
-            writer = new StreamWriter(stream);
+            this.encrypted = encrypt;
+            this.compressed = compress;
+            this.key = GenerateKeyOrIv(32);
+            this.iv = GenerateKeyOrIv(16);
         }
 
         public void Write(string text)
         {
+            Stream outputStream = stream;
+            if (encrypted)
+            {
+                Aes aes = Aes.Create();
+                ICryptoTransform encryptor = aes.CreateEncryptor(this.key, this.iv);
+                outputStream = new CryptoStream(stream, encryptor, CryptoStreamMode.Write);
+            }
+
+            if (compressed)
+            {
+                outputStream = new GZipStream(outputStream, CompressionMode.Compress);
+            }
+
+            StreamWriter writer = new StreamWriter(outputStream);
             writer.WriteLine(text);
             writer.Flush();
             stream.Position = 0;
@@ -24,8 +45,30 @@ namespace WorkWithStream
         public string Read()
         {
             stream.Position = 0;
+
+            Stream inputStream = stream;
+            if (this.compressed)
+            {
+                inputStream = new GZipStream(stream, CompressionMode.Decompress);
+            }
+
+            if (this.encrypted)
+            {
+                Aes aes = Aes.Create();
+                ICryptoTransform decryptor = aes.CreateDecryptor(this.key, this.iv);
+                inputStream = new CryptoStream(inputStream, decryptor, CryptoStreamMode.Read);
+            }
+
+            StreamReader reader = new StreamReader(inputStream);
             return reader.ReadToEnd();
+        }
+
+        private byte[] GenerateKeyOrIv(int size)
+        {
+            byte[] array = new byte[size];
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            rng.GetBytes(array);
+            return array;
         }
     }
 }
-
--- StreamWriteAndRead/StreamWriteAndRead/streamTests.cs
+++ StreamWriteAndRead/StreamWriteAndRead/streamTests.cs
@@ -1,3 +1,7 @@
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace WorkWithStream
 {
     public class streamTests
@@ -7,7 +11,7 @@ namespace WorkWithStream
         {
             string text = "Am reusit";
             string resultText = text + "\r" + "\n";
-            var str = new StreamWriteAndRead(new MemoryStream());
+            var str = new StreamWriteAndRead(new MemoryStream(), false, false);
             str.Write(text);
             Assert.Equal(resultText, str.Read());
 
@@ -18,21 +22,20 @@ namespace WorkWithStream
         {
             string text = "Am reusit \n sa scriu \n in fisier";
             string resultText = text + "\r" + "\n";
-            var str = new StreamWriteAndRead(new MemoryStream());
+            var str = new StreamWriteAndRead(new MemoryStream(), false, false);
             str.Write(text);
             Assert.Equal(resultText, str.Read());
 
         }
 
         [Fact]
-        public void WriteATextInANewFileCreateByProject()
+        public void WriteAEncryptedText()
         {
             string text = "Am reusit \n sa scriu \n in fisier";
             string resultText = text + "\r" + "\n";
-            var str = new StreamWriteAndRead(new MemoryStream());
+            var str = new StreamWriteAndRead(new MemoryStream(), true, false);
             str.Write(text);
             Assert.Equal(resultText, str.Read());
-
         }
     }
 }
