a283b2853e9cd2bafee979b5f586d8de591939af
Vlad4774 <vladbaracut7@yahoo.com>
2023-01-09 13:03:57 +0200

    compress and encrypted

--- StreamWriteAndRead/StreamWriteAndRead/streamTests.cs
+++ StreamWriteAndRead/StreamWriteAndRead/streamTests.cs
@@ -37,5 +37,25 @@ namespace WorkWithStream
             str.Write(text);
             Assert.Equal(resultText, str.Read());
         }
+
+        [Fact]
+        public void WriteACompressedText()
+        {
+            string text = "Am reusit \n sa scriu \n in fisier";
+            string resultText = text + "\r" + "\n";
+            var str = new StreamWriteAndRead(new MemoryStream(), false, true);
+            str.Write(text);
+            Assert.Equal(resultText, str.Read());
+        }
+
+        [Fact]
+        public void WriteACompressedAndEncryptedText()
+        {
+            string text = "Am reusit \n sa scriu \n in fisier";
+            string resultText = text + "\r" + "\n";
+            var str = new StreamWriteAndRead(new MemoryStream(), true, true);
+            str.Write(text);
+            Assert.Equal(resultText, str.Read());
+        }
     }
 }
