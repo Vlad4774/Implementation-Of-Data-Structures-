8fc25aa07ef5adfcdd4809a62b9d7fd299c64f6a
Vlad4774 <vladbaracut7@yahoo.com>
2023-02-02 17:13:53 +0200

    method Any

--- linq/linq/Operations.cs
+++ linq/linq/Operations.cs
@@ -12,7 +12,21 @@ namespace linq
                     return false;
                 }
             }
+            
             return true;
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
         }
     }
 }
--- linq/linq/operationsTests.cs
+++ linq/linq/operationsTests.cs
@@ -20,5 +20,22 @@ namespace linq
             Assert.False(result);
         }
 
+        [Fact]
+        public void AnyElementsSatisfyingPredicateReturnsTrue()
+        {
+            string[] strings = { "Hello", "World", "Goodbye" };
+            Func<string, bool> predicate = s => s.Length > 5;
+            bool result = strings.Any(predicate);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NoneOfElementsSatisfyingPredicateReturnsFalse()
+        {
+            string[] strings = { "Hello", "World", "yes" };
+            Func<string, bool> predicate = s => s.Length > 10;
+            bool result = strings.Any(predicate);
+            Assert.False(result);
+        }
     }
 }
