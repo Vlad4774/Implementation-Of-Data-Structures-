6203714b15b6d0cb60504a1842188039ff2f3c3b
Vlad4774 <vladbaracut7@yahoo.com>
2023-02-12 22:11:25 +0200

    class numberofVowlesAndConsonant

---
+++ linq/linq/NumberOfVowelAndConsonant.cs
@@ -0,0 +1,23 @@
+﻿
+namespace linq
+{
+    class NumberOfVowelAndConsonant
+    {
+        private string text;
+
+        public NumberOfVowelAndConsonant(string text)
+        {
+            this.text = text.ToLower();
+        }
+
+        public int ReturnNumberOfVowels()
+        {
+            return text.Count(character => "aeiou".Contains(character));
+        }
+
+        public int ReturnNumberOfConsonants()
+        {
+            return text.Count(character => "bcdfghjklmnpqrstvwxyz".Contains(character));
+        }
+    }
+}
---
+++ linq/linq/NumberOfVowelsAndConsonantTests.cs
@@ -0,0 +1,22 @@
+﻿
+namespace linq
+{
+    public class NumberOfVowelsAndConsonantTests
+    {
+        [Fact]
+        public void ReturnNumberOfVowels()
+        {
+            var numbers = new NumberOfVowelAndConsonant("ana are mere");
+            Assert.Equal(6, numbers.ReturnNumberOfVowels());
+            Assert.Equal(4, numbers.ReturnNumberOfConsonants());
+        }
+
+        [Fact]
+        public void ReturnNumberOfConsonants()
+        {
+            var numbers = new NumberOfVowelAndConsonant("Hello World");
+            Assert.Equal(7, numbers.ReturnNumberOfConsonants());
+            Assert.Equal(3, numbers.ReturnNumberOfVowels());
+        }
+    }
+}
