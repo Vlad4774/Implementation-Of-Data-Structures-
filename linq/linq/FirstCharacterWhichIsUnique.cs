5db8ec613cf6fb2552ca8ac836dfec301f3d152b
Vlad4774 <vladbaracut7@yahoo.com>
2023-02-12 22:24:39 +0200

    firstCharacterWhichIsUnique

---
+++ linq/linq/FirstCharacterWhichIsUnique.cs
@@ -0,0 +1,18 @@
+﻿
+namespace linq
+{
+    class FirstCharacterWhichIsUnique
+    {
+        string text;
+        public FirstCharacterWhichIsUnique(string text)
+        {
+            this.text = text;
+        }
+
+        public char GetFirstUniqueCharacter()
+        {
+            var uniqueChars = text.GroupBy(c => c).Where(g => g.Count() == 1);
+            return uniqueChars.First().Key;
+        }
+    }
+}
---
+++ linq/linq/FirstCharacterWhichIsUniqueTests.cs
@@ -0,0 +1,17 @@
+﻿
+namespace linq
+{
+    public class FirstElementWhichIsUniqueTests
+    {
+        [Fact]
+        public void GetFirstUniqueCharacter()
+        {
+            string text = "hello world";
+            var firstElement = new FirstCharacterWhichIsUnique(text);
+
+            char result = firstElement.GetFirstUniqueCharacter();
+
+            Assert.Equal('h', result);
+        }
+    }
+}
