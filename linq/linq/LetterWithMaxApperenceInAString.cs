0e34cc7536983a440943f6f58ede3c37fdff3707
Vlad4774 <vladbaracut7@yahoo.com>
2023-03-01 12:05:15 +0200

    LetterWithMaxApperenceInAString

---
+++ linq/linq/LetterWithMaxApperenceInAString.cs
@@ -0,0 +1,20 @@
+﻿
+namespace linq
+{
+    class LetterWithMaxApperenceInAString
+    {
+        string givenString;
+        public LetterWithMaxApperenceInAString(string givenString) 
+        {
+            this.givenString = givenString;
+        }
+
+        public char LetterWithMaxApperence()
+        {
+            var grouped = givenString.GroupBy(c => c);
+            var ordered = grouped.OrderBy(c => c.Count());
+
+            return ordered.Last().Key;
+        }
+    }
+}
---
+++ linq/linq/LetterWithMaxApperenceInAStringTests.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace linq
+{
+    public class LetterWithMaxApperenceInAStringTests
+    {
+        [Fact]
+        public void ReturnsCorrectLetter()
+        {
+            var givenString = new LetterWithMaxApperenceInAString("Vlllad");
+            Assert.Equal(givenString.LetterWithMaxApperence(), 'l');
+        }
+
+        [Fact]
+        public void ReturnsCorrectLetterInALongString()
+        {
+            var givenString = new LetterWithMaxApperenceInAString("Ana are mere, portocale, aaaa, aur, atc");
+            Assert.Equal(givenString.LetterWithMaxApperence(), 'a');
+        }
+    }
+}
