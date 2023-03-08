faf02e7e36c365d9c8be83c467e4f5df111514c4
Vlad4774 <vladbaracut7@yahoo.com>
2023-03-08 16:45:53 +0200

    palindrom

---
+++ linq/linq/palindromTests.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace linq
+{
+    public class palindromTests
+    {
+        [Fact]
+        public void ReturnsCorrectPalindrom()
+        {
+            var givenString = new palindrom("aba");
+            var result = givenString.AllPalindroms();
+            var expected = new List<string> { "a", "aba", "a" };
+            Assert.Equal(expected, result);
+        }
+    }
+}
